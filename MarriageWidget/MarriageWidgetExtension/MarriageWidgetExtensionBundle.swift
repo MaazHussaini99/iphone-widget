9827992bd3c62c8ab2d8868d30d5268b81e34e10
MaazHussaini99 <maazhussaini@outlook.com>
2025-12-02 23:30:17 -0600

    transgorm inot xcode

---
+++ MarriageWidget/MarriageWidgetExtension/MarriageWidgetExtensionBundle.swift
@@ -0,0 +1,16 @@
+//
+//  MarriageWidgetExtensionBundle.swift
+//  MarriageWidgetExtension
+//
+//  Created by Maaz Hussaini on 12/2/25.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct MarriageWidgetExtensionBundle: WidgetBundle {
+    var body: some Widget {
+        MarriageWidgetExtension()
+    }
+}
